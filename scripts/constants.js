c63abc81b1c587285bc336b227daac9367011e87
Maximilian Marshak <maximilian.marshak@gmail.com>
2023-05-01 19:40:49 +0300

    feat: create two lines of keyboard

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,42 +1,250 @@
 /*
-  background: 'black', 'gray'
+  background: 'black' = #222222, 'gray' = #444444,
   width: '50px', '60px', '125px', '75px', '300px'
  */
 
   const FIRST_EN_LINE = [
     {
-      background: 'black',
+      background: '#444444',
       width: '50px',
       key: '`',
       code: 'Backquote',
       isUpperCase: false,
     },
     {
-      background: 'black',
+      background: '#444444',
       width: '50px',
       key: '1',
       code: 'Digit1',
       isUpperCase: false,
     },
-  ]
+    {
+        background: '#444444',
+        width: '50px',
+        key: '2',
+        code: 'Digit2',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '3',
+        code: 'Digit3',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '4',
+        code: 'Digit4',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '5',
+        code: 'Digit5',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '6',
+        code: 'Digit6',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '7',
+        code: 'Digit7',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '8',
+        code: 'Digit8',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '9',
+        code: 'Digit9',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '0',
+        code: 'Digit0',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '-',
+        code: 'Minus',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '-',
+        code: 'Minus',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '=',
+        code: 'Equal',
+        isUpperCase: false,
+      },
+      {
+        background: '#222222',
+        width: '124px',
+        key: 'Backspace',
+        code: 'Backspace',
+        isUpperCase: false,
+      },
+  ];
+
   const SECOND_EN_LINE = [
     {
-      background: 'black',
+      background: '#222222',
       width: '60px',
       key: 'Tab',
       code: 'Tab',
       isUpperCase: false,
     },
     {
-      background: 'gray',
+      background: '#444444',
       width: '50px',
       key: 'q',
       code: 'KeyQ',
-      isUpperCase: true,
+      isUpperCase: false,
     },
+    {
+        background: '#444444',
+        width: '50px',
+        key: 'w',
+        code: 'KeyW',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'e',
+        code: 'KeyE',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'r',
+        code: 'KeyR',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 't',
+        code: 'KeyT',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'y',
+        code: 'KeyY',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'u',
+        code: 'KeyU',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'i',
+        code: 'KeyI',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'o',
+        code: 'KeyO',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'p',
+        code: 'KeyP',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'p',
+        code: 'KeyP',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '[',
+        code: 'BracketLeft',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: ']',
+        code: 'BracketRight',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: '\\',
+        code: 'Backslash',
+        isUpperCase: false,
+      },
+      {
+        background: '#222222',
+        width: '60px',
+        key: 'Del',
+        code: 'Delete',
+        isUpperCase: false,
+      }
+  ];
+
+  const THIRD_EN_LINE = [
+    {
+        background: '#222222',
+        width: '124px',
+        key: 'CapsLock',
+        code: 'CapsLock',
+        isUpperCase: false,
+      },
+      {
+        background: '#444444',
+        width: '50px',
+        key: 'a',
+        code: 'KeyA',
+        isUpperCase: false,
+      },
   ]
   
   export const KEYBOARD_EN = [
     FIRST_EN_LINE,
     SECOND_EN_LINE,
+    THIRD_EN_LINE
   ]
